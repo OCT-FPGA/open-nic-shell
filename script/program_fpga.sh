a33fb0b806e2b348e748fe5823c946eab5a3490b
Zhaoyang <function047@gmail.com>
2023-06-27 14:47:09 -0400

    Log: Edit readme

--- script/program_fpga.sh
+++ script/program_fpga.sh
@@ -4,9 +4,9 @@
 
 echo $#
 if [[ $# -le 2 ]] || [[ -z EXTENDED_DEVICE_BDF1 ]] || [[ -z $XILINX_VIVADO ]]; then
-    echo "Usage: EXTENDED_DEVICE_BDF1=<EXTENDED_DEVICE_BDF1> program_fpga.sh BITSTREAM_PATH BOARD jtag_id "
+    echo "Usage: EXTENDED_DEVICE_BDF1=<EXTENDED_DEVICE_BDF1> ./program_fpga.sh BITSTREAM_PATH BOARD jtag_id "
     echo "Please export EXTENDED_DEVICE_BDF1 and [EXTENDED_DEVICE_BDF2 (if needed for 2 port boards)]"
-    echo "Example: EXTENDED_DEVICE_BDF1=<0000:86:00.0> program_device.sh BITSTREAM_PATH BOARD jtag_id "
+    echo "Example: EXTENDED_DEVICE_BDF1=0000:86:00.0 ./program_fpga.sh BITSTREAM_PATH BOARD jtag_id "
     echo "Please ensure vivado is loaded into system path."
     exit 1
 fi
